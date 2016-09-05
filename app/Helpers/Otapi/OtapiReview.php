2a0e8d59eabd9beb69cbbf3a2c489431e779e4aa
Alexandr Fanamurov <fanamurov@ya.ru>
2016-09-05 18:17:39 +1000

    Фиксы

--- app/Helpers/Otapi/OtapiReview.php
+++ app/Helpers/Otapi/OtapiReview.php
@@ -4,19 +4,27 @@ namespace App\Helpers\Otapi;
 
 class OtapiReview
 {
-	/** Установка флага на игнорирование ошибок api от конкретного запроса @var  bool */
-	protected $allow_safe_mode = TRUE;
+    /** Установка флага на игнорирование ошибок api от конкретного запроса @var  bool */
+    protected $allow_safe_mode = TRUE;
 
-	public function get($itemId, $framePosition = 0, $frameSize = 10)
-	{
-		$otapiConnection = new OtapiConnection;
-		$data = $otapiConnection->create_request('GetTradeRateInfoListFrame', [
-			'itemId' => $itemId,
-			'framePosition' => $framePosition,
-			'frameSize' => $frameSize], $this->allow_safe_mode);
+    public function get($itemId, $framePosition = 0, $frameSize = 10)
+    {
+        $otapiConnection = new OtapiConnection;
+        $data = $otapiConnection->create_request('GetTradeRateInfoListFrame', [
+            'itemId' => $itemId,
+            'framePosition' => $framePosition,
+            'frameSize' => $frameSize], $this->allow_safe_mode);
         if(isset($data->TradeRateInfoList)){
-            return $data->TradeRateInfoList;
+            return $this->normalizeGet($data->TradeRateInfoList);
         }
         return collect();
-	}
+    }
+
+    protected function normalizeGet($item)
+    {
+        if($item->TotalCount === '1'){
+            $item->Content->Item = [0 => $item->Content->Item];
+        }
+        return $item;
+    }
 }
--- resources/views/tbkhv/otapi/search.blade.php
+++ resources/views/tbkhv/otapi/search.blade.php
@@ -88,51 +88,10 @@
             @if($data->Items->Items->TotalCount > 0)
                 @if( !isset($data->Items->Items->Content->Item->Id))
                     @foreach($data->Items->Items->Content->Item as $data_value)
-                        <div class="col-xs-12 col-sm-6 item-catalog link_block_this" data-href='/otapi/{{ $data_value->CategoryId }}/tovar/{{ $data_value->Id }}'>
-                            <div class="div-img">
-                                @if(isset($data_value->Pictures))
-                                    @if(is_array($data_value->Pictures->ItemPicture))
-                                        <img class="all-width" src="{{ $data_value->Pictures->ItemPicture[0]->Medium }}" alt="{{ $data_value->OriginalTitle }}">
-                                    @else
-                                        <img class="all-width" src="{{ $data_value->Pictures->ItemPicture->Medium }}" alt="{{ $data_value->OriginalTitle }}">
-                                    @endif
-                                @endif
-                            </div>
-                            <p class="cost">{{ $data_value->Price->ConvertedPriceWithoutSign }} {{ $data_value->Price->CurrencySign }}</p>
-                            <p><a class="title-tovar" href="/otapi/{{ $data_value->CategoryId }}/tovar/{{ $data_value->Id }}">
-                                    {{ mb_strimwidth($data_value->Title, 0, 70, '...') }}
-                                </a></p>
-                            <p class="vendor">{{ $data_value->VendorName }}
-                                @php($score_delim = ceil($data_value->VendorScore/5))
-                                <br/>@for($i=0; $i < $score_delim; $i++)
-                                    <i class="fa fa-star"></i>
-                                @endfor
-                            </p>
-                        </div>
+                        @include('tbkhv.otapi.item.block-tovar', ['data_value' => $data_value])
                     @endforeach
                 @else
-                    @php($data_value = $data->Items->Items->Content->Item)
-                    <div class="col-xs-12 col-sm-6 item-catalog link_block_this" data-href='/otapi/{{ $data_value->CategoryId }}/tovar/{{ $data_value->Id }}'>
-                        <div class="div-img">
-                            @if(isset($data_value->Pictures))
-                                @if(is_array($data_value->Pictures->ItemPicture))
-                                    <img class="all-width" src="{{ $data_value->Pictures->ItemPicture[0]->Medium }}" alt="{{ $data_value->OriginalTitle }}">
-                                @else
-                                    <img class="all-width" src="{{ $data_value->Pictures->ItemPicture->Medium }}" alt="{{ $data_value->OriginalTitle }}">
-                                @endif
-                            @endif
-                        </div>
-                        <p class="cost">{{ $data_value->Price->ConvertedPrice }}</p>
-                        <p><a class="title-tovar" href="/otapi/{{ $data_value->CategoryId }}/tovar/{{ $data_value->Id }}">
-                                {{ mb_strimwidth($data_value->Title, 0, 70, '...') }}
-                            </a></p>
-                        <p class="vendor">{{ $data_value->VendorName }}
-                            @php($score_delim = ceil($data_value->VendorScore/5))
-                            <br/>@for($i=0; $i < $score_delim; $i++)
-                                <i class="fa fa-star"></i>
-                            @endfor
-                        </p>
-                    </div>
+                    @include('tbkhv.otapi.item.block-tovar', ['data_value' => $data->Items->Items->Content->Item])
                 @endif
             @else
                 <div class="alert alert-danger">К сожалению, по данному поисковому запросу ничего не нашлось, попробуйте изменить данные для поиска</div>
